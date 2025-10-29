f348d222df3acd49d621063bfe6768cea0be181c
NOLi7000 <majin.dmx00000@gmail.com>
2025-10-29 15:08:55 +0300

    first commit

--- Cars.java
+++ Cars.java
@@ -1,6 +1,6 @@
 class toyota {
     final void displayCarColor() {
-        System.out.println("toyotaa cars have seats ");
+        System.out.println("toyotaa carss have seats ");
     }
 }
 class wish extends toyota {
